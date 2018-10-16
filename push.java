4575d0e69af02660d93fd47bd83d27283be82424
littlepazienza <bjpazienza@loyola.edu>
2018-10-16 11:50:11 -0400

    tried treeing

--- push.java
+++ push.java
@@ -2,7 +2,7 @@
 abstract class ExpressionTree 
 {
   public abstract int evaluate();
-  ...
+	public abstract boolean canEvaluate();
 }
 
 class Number extends ExpressionTree
@@ -11,7 +11,62 @@ class Number extends ExpressionTree
 
   public Number(int v) { value = v; } 
   public int evaluate() { return value; } 
-  ...
+  public boolean canEvaluate() { return false; }
 }
+
+class Operation extends ExpressionTree
+{
+	protected String value;
+	protected ExpressionTree left, right
+
+	public Operation( String v, ExpressionTree l, ExpressionTree r )
+	{
+		value = v;
+		left = l;
+		right = r;
+	}
+
+	public int evaluate()
+	{
+		switch(value)
+		{
+			case "+":return left.evaluate() + right.evaluate();
+			case "-":return left.evaluate() - right.evaluate();
+			case "*":return left.evaluate() * right.evaluate();
+			case "/":return left.evaluate() / right.evaluate();
+			case "neg":return -1; //TODO
+			case "abs":return Math.abs(-1); //TODO
+			default: reutnr -1;
+		}
+	}
+
+	public boolean canEvaluate() { return true; }
+}
+
+class Tree
+{
+	protected Operation root;
+
+	public Tree()
+	{
+		root = null;
+	}
+
+	public void addOperation(Operation o)
+	{
+		findSpotForOperation(root, o);
+	}
+
+	//parses through tree finds first open spot for operation and adds it
+	private boolean findSportForOperation( Operation r, Operation o)
+	{
+		if(isFull())
+			return false;
+
+			
+	}
+
+
+
 
 
